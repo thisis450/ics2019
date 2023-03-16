706d9419f824727b74dab592122ce45e261ff8fc
2012159-Jin Leqing <1185056784@qq.com>
2023-03-16 22:32:55 +0800

    位于分支 pa1  要提交的变更： 	修改：     src/monitor/debug/ui.c

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -39,7 +39,7 @@ static int cmd_q(char *args) {
 static int cmd_help(char *args);
 
 static int cmd_si(char *args) {
-  char arg=strtok(args," ");
+  char* arg=strtok(args," ");
   int N=0;
   if(arg==NULL)
   {
