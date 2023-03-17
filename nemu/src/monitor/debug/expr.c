8ceed70314b2685b307fd2f6b1477e518abce102
tracer-ics2019 <tracer@njuics.org>
2023-03-17 22:17:50 +0800

    >  compile 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:17:50 up 1 day, 22:54,  1 user,  load average: 0.27, 0.08, 0.02 32ca8616ba081d055380073d1fb14c5c3d956694

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -129,7 +129,7 @@ static bool make_token(char *e) {
             break;
 
 
-          default: TODO();
+          //default: TODO();
         }
 
         break;
@@ -324,7 +324,7 @@ uint32_t expr(char *e, bool *success) {
     *success = false;
     return 0;
   }
-  return eval(0,nr_token);
+  return eval(0,nr_token-1);
 
   /* TODO: Insert codes to evaluate the expression. */
   TODO();
