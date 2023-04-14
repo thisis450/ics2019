9c2fd7360fdc7b29e62694217eac93b121aad282
tracer-ics2019 <tracer@njuics.org>
2023-04-14 23:44:53 +0800

    >  compile 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:44:53 up 30 days, 21 min,  1 user,  load average: 0.40, 0.08, 0.03 b4734cd7e22c8c9d90b4af4e636bd64013e5a2

--- nemu/src/isa/x86/exec/control.c
+++ nemu/src/isa/x86/exec/control.c
@@ -26,8 +26,9 @@ make_EHelper(jmp_rm) {
 
 make_EHelper(call) {
   // the target address is calculated at the decode stage
-  TODO();
-
+  //TODO();
+  rtl_push(pc);
+  rtl_j(decinfo.jmp_pc);
   print_asm("call %x", decinfo.jmp_pc);
 }
 
