0bbe608b06aa9316f52518fadaf86b3fe12753a7
2012159-Jin Leqing <1185056784@qq.com>
2023-04-15 12:34:49 +0800

    位于分支 pa2  要提交的变更： 	修改：     nemu/src/isa/x86/exec/arith.c 	修改：     nemu/src/isa/x86/exec/exec.c

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -7,9 +7,15 @@ make_EHelper(add) {
 }
 
 make_EHelper(sub) {
-  TODO();
-
-  print_asm_template2(sub);
+  //TODO();
+	rtl_sub(&s0,&id_dest->val,&id_src->val);
+	rtl_update_ZFSF(&s0,id_dest->width);
+	rtl_is_sub_carry(&s1,&s0,&id_dest->val);
+	rtl_set_CF(&s1);
+	rtl_is_sub_overflow(&s1,&s0,&id_dest->val,&id_src->val,id_dest->width);
+	rtl_set_OF(&s1);
+	operand_write(id_dest,&s0);
+	print_asm_template2(sub);
 }
 
 make_EHelper(cmp) {
