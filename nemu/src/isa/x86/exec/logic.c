2778c9950e70c33df3d2fd08a606a928f00a0e9c
tracer-ics2019 <tracer@njuics.org>
2023-04-15 13:42:31 +0800

    >  compile 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:42:31 up 30 days, 14:19,  1 user,  load average: 0.01, 0.05, 0.01 d9e3c774aa7637753eaa689050a5a17339cd3215

--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -14,9 +14,14 @@ make_EHelper(and) {
 }
 
 make_EHelper(xor) {
-  TODO();
-
-  print_asm_template2(xor);
+  //TODO();
+	rtl_li(&s1,0);
+	rtl_set_OF(&s1);
+	rtl_set_CF(&s1);
+	rtl_xor(&s0,&id_dest->val,&id_src->val);
+	rtl_update_ZFSF(&s0,id_dest->width);
+	operand_write(id_dest,&s0);
+	print_asm_template2(xor);
 }
 
 make_EHelper(or) {
