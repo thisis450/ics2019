5b44a275fb76e09d94d5586321ea7fa916f3e4e6
tracer-ics2019 <tracer@njuics.org>
2023-04-15 23:59:25 +0800

    >  run 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:59:25 up  9:09,  1 user,  load average: 0.01, 0.05, 0.04 954982087a89a64d423a90909d6ad7522f3b0021

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -12,7 +12,39 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+  va_list sArgv;
+  int ret=0,val_d,len,tmp;
+  char ds[15];
+  char* val_s;
+  va_start(sArgv,fmt);
+  for (;;)
+  {
+	while (*fmt!='%'&&*fmt!='\0') *out=*fmt,++fmt,++out,++ret;
+	if (*fmt=='\0') {*out='\0';break;}
+	if (*(fmt+1)=='d')
+	{
+		val_d=va_arg(sArgv,int);
+		if (val_d<0) *out='-',++out,++ret;
+		len=0;
+		do
+		{
+			tmp=val_d%10;
+			ds[++len]=(tmp<0?-tmp:tmp)+'0';
+			val_d/=10;
+		}while (val_d!=0);
+		for (int i=len;i>=1;--i) *out=ds[i],++out;
+		ret+=len;
+		fmt=fmt+2;
+	}
+	else if (*(fmt+1)=='s')
+	{
+		val_s=va_arg(sArgv,char*);
+		while (*val_s!='\0') *out=*val_s,++val_s,++out,++ret;
+		fmt=fmt+2;
+	}
+  }
+  va_end(sArgv);
+  return ret==0?-1:ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
