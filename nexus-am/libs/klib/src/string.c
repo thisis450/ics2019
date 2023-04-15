caf5e63c730d17ac04242f953076fc89e1d2ffbf
tracer-ics2019 <tracer@njuics.org>
2023-04-15 23:36:06 +0800

    >  run 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:36:06 up  8:46,  1 user,  load average: 0.09, 0.05, 0.03 13c24f9e3419918246b1b749a0670fcc57f2f78e

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,39 +3,79 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+  size_t len=1;
+  while (s[len-1]!='\0') ++len;
+  return len-1;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+	char* pd=dst;
+	while (*src!='\0') *pd=*src,++pd,++src;
+	return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+	char* pd=dst;
+	while (n>0&&*src!='\0') --n,*pd=*src,++pd,++src;
+	while (n>0) --n,*pd='\0',++pd;
+	return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+	char* pd=dst;
+	while (*pd!='\0') ++pd;
+	while (*src!='\0') *pd=*src,++pd,++src;
+	*pd='\0';
+	return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+	while (*s1!='\0'&&*s2!='\0'&&*s1==*s2) ++s1,++s2;
+	if (*s1>*s2) return 1;
+	else if (*s1<*s2) return -1;
+	else return 0;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
+	while (n>0)
+	{
+		if (*s1=='\0'&&*s2=='\0') return 0;
+		if (*s1!=*s2) return *s1>*s2?1:-1;
+		--n;++s1;++s2;
+	}
+	return 0;
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+	unsigned char val=c;
+	char *ptr=(char *)v;
+	while (n>0)
+	{
+		*ptr=val;
+		++ptr;
+		--n;
+	}
+	return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+	char *ptr=(char *)out,*pi=(char *)in;
+	while (n>0)
+	{
+		*ptr=*pi;
+		++ptr;++pi;--n;
+	}
+	return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
-  return 0;
+	char *p1=(char *)s1,*p2=(char*)s2;
+	while (n>0)
+	{
+		if (*p1!=*p2) return *p1>*p2?1:-1;
+		--n;++p1;++p2;
+	}
+	return 0;
 }
 
 #endif
