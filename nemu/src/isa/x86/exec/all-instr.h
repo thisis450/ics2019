d3110f7cefa6b4465fc20b85b97c5a1109853393
tracer-ics2019 <tracer@njuics.org>
2023-04-17 20:46:56 +0800

    >  compile 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:46:56 up 21:35,  1 user,  load average: 0.71, 0.46, 0.26 f13ba0841b9ec120fdd9196d2b63ffc115a7db26

--- nemu/src/isa/x86/exec/all-instr.h
+++ nemu/src/isa/x86/exec/all-instr.h
@@ -6,33 +6,6 @@ make_EHelper(operand_size);
 
 make_EHelper(inv);
 make_EHelper(nemu_trap);
-// call & ret
-make_EHelper(ret);
-make_EHelper(call);
-make_EHelper(call_rm);
-// data-mov
-make_EHelper(push);
-make_EHelper(pop);
-make_EHelper(pusha);
-make_EHelper(popa);
-make_EHelper(leave);
-make_EHelper(lea);
-make_EHelper(movzx);
-make_EHelper(movsx);
-make_EHelper(cwtl);
-make_EHelper(cltd);
-// special
-make_EHelper(nop);
-// logic
-make_EHelper(test);
-make_EHelper(and);
-make_EHelper(xor);
-make_EHelper(or);
-make_EHelper(sar);
-make_EHelper(shl);
-make_EHelper(shr);
-make_EHelper(setcc);
-make_EHelper(not);
 // arith
 make_EHelper(add);
 make_EHelper(sub);
@@ -48,10 +21,34 @@ make_EHelper(imul2);
 make_EHelper(imul3);
 make_EHelper(div);
 make_EHelper(idiv);
-// control
+make_EHelper(ret);
+make_EHelper(call);
+make_EHelper(call_rm);
 make_EHelper(jcc);
 make_EHelper(jmp);
 make_EHelper(jmp_rm);
-//system
+make_EHelper(push);
+make_EHelper(pop);
+make_EHelper(pusha);
+make_EHelper(popa);
+make_EHelper(leave);
+make_EHelper(lea);
+make_EHelper(movzx);
+make_EHelper(movsx);
+make_EHelper(cwtl);
+make_EHelper(cltd);
+make_EHelper(test);
+make_EHelper(and);
+make_EHelper(xor);
+make_EHelper(or);
+make_EHelper(sar);
+make_EHelper(shl);
+make_EHelper(shr);
+make_EHelper(setcc);
+make_EHelper(not);
+make_EHelper(nop);
+
+
+
 make_EHelper(in);
 make_EHelper(out);
