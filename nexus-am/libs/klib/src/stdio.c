b7925115ede7e4e068ee0fe46da3f8678cfab452
tracer-ics2019 <tracer@njuics.org>
2023-04-18 12:40:08 +0800

    >  run 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:40:08 up 1 day, 13:29,  1 user,  load average: 0.04, 0.13, 0.08 ba7d285647c7c63c4d953a126a61b7dcf8c0cb6

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,281 +3,151 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-#define isdigit(x) (((x) >= '0') && ((x) <= '9'))
-#define todigit(x) ((x) - '0')
-#define LEFT 0x01
-#define PLUS 0x02
-#define SPACE 0x04
-#define SPECIAL 0x08
-#define ZERO 0x10
-#define SIGN 0x20	 /* signed if set */
-#define SMALL 0x40 /* 'abcdef' if set, 'ABCDEF' otherwise */
-
-int32_t get_wide(const char **s);
-void number_to_string(long num, int32_t base, int32_t flags, int32_t wide, int32_t precision, char **s);
-
 int printf(const char *fmt, ...) {
-  va_list ap;
-	va_start(ap, fmt);
-	char out[2000];
-	int len = vsprintf(out, fmt, ap);
-	for(int i = 0;i < len; i++){
-		_putc(out[i]);
+	va_list sArgv;
+	int ret=0,val_d,len,tmp;
+	unsigned val_u=0;
+	char ds[35];
+	char* val_s;
+	va_start(sArgv,fmt);
+	for (;;)
+	{
+		while (*fmt!='%'&&*fmt!='\0') _putc(*fmt++),++ret;
+		if (*fmt=='\0') {_putc('\0');++ret;break;}
+		switch (*(fmt+1))
+		{
+			case 'd':
+			{
+				val_d=va_arg(sArgv,int);
+				if (val_d<0) _putc('-'),++ret;
+				len=0;
+				do
+				{
+					tmp=val_d%10;
+					ds[++len]=(tmp<0?-tmp:tmp)+'0';
+					val_d/=10;
+				}while(val_d);
+				for (int i=len;i>=1;--i) _putc(ds[i]);
+				ret+=len;
+				break;
+			}
+			case 's':
+			{
+				val_s=va_arg(sArgv,char*);
+				while (*val_s!='\0') _putc(*val_s),val_s++;
+				break;
+			}
+			case 'u':
+			{
+				val_u=va_arg(sArgv,unsigned);
+				len=0;
+				do
+				{
+					tmp=val_u%10;
+					ds[++len]=tmp+'0';
+					val_u/=10;
+				}while(val_u);
+				for (int i=len;i>=1;--i) _putc(ds[i]);
+				ret+=len;
+				break;
+			}
+			case 'x':
+			{
+				val_u=va_arg(sArgv,unsigned);
+				len=0;
+				do
+				{
+					tmp=val_u%16;
+					ds[++len]=(tmp<10?tmp+'0':tmp-9+'A'-1);
+					val_u/=16;
+				}while(val_u);
+				for (int i=len;i>=1;--i) _putc(ds[i]);
+				ret+=len;
+				break;
+			}
+
+		}
+		fmt=fmt+2;
 	}
-	return len;
+	va_end(sArgv);
+	return ret==0?-1:ret;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  char c;
-	char *start = out;
-	int32_t flags;
-	int32_t wide;
-	int32_t precision;
-	int32_t qualifier;
-	char *s;
-	int32_t i, len, base;
-	while((c = *fmt++) != 0){
-		/* normal string */
-		if(c != '%'){
-			*out++ = c;
-			continue;
-		}
-		/* %%, just a formatted '%' */
-		if(*fmt == '%'){
-			*out++ = '%';
-			continue;
-		}
-		/* % used to indicate a formatted string */
-		flags = 0; /* get flags */
-		while(1){
-			if(*fmt == '-'){ flags |= LEFT; fmt++; continue;}
-			if(*fmt == '+'){ flags |= PLUS; fmt++; continue;}
-			if(*fmt == ' '){ flags |= SPACE; fmt++; continue;}
-			if(*fmt == '#'){ flags |= SPECIAL; fmt++; continue;}
-			if(*fmt == '0'){ flags = ZERO; fmt++; continue;}
-			break;
-		}
-		/* get wide */
-		wide = -1;
-		if(isdigit(*fmt)){
-			wide = get_wide((const char **)(&fmt)); /* read from the range of fmt which indicates the width */
-		}
-		else if(*fmt == '*'){
-			wide = va_arg(ap, int32_t);
-			fmt++;
-		}
-		/* get precision */
-		precision = -1;
-		if(*fmt == '.'){
-			fmt++;
-			if(isdigit(*fmt)){
-				precision = get_wide((const char **)(&fmt));
-			}
-			else if(*fmt == '*'){
-				precision = va_arg(ap, int32_t);
-				fmt++;
-			}
-			else precision = 0;
-		}
-		/* get qualifier */
-		qualifier = -1;
-		if((*fmt == 'h') || (*fmt == 'l')) qualifier = *fmt++;
-		/* get format */
-		switch(*fmt++){
-			case 'i':
-			case 'd':
-				flags |= SIGN;
-				if(precision != -1) flags &= ~ZERO;
-				switch(qualifier){
-					case 'h':
-						number_to_string((short)va_arg(ap, int32_t), 10, flags, wide, precision, &out);
-						break;
-					case 'l':
-						number_to_string(va_arg(ap, long), 10, flags, wide, precision, &out);
-						break;
-					default:
-						number_to_string(va_arg(ap, int32_t), 10, flags, wide, precision, &out);
-						break;
-				}
-				break;
-			case 'u':
-				base = 10;
-				goto num_to_str_without_sign;
-			case 'o':
-				base = 8;
-				goto num_to_str_without_sign;
-			case 'x':
-				flags |= SMALL;
-			case 'X':
-				base = 16;
-
-				num_to_str_without_sign:
-				flags &= (~PLUS & ~SPACE);
-				if(precision != -1) flags &= ~ZERO;
-				switch(qualifier){
-					case 'h':
-						number_to_string((unsigned short)va_arg(ap, int32_t), base, flags, wide, precision, &out);
-						break;
-					case 'l':
-						number_to_string((unsigned long)va_arg(ap, long), base, flags, wide, precision, &out);
-						break;
-					default:
-						number_to_string((uint32_t)va_arg(ap, int32_t), base, flags, wide, precision, &out);
-						break;
-				}
-				break;
-
-			case 's':
-				s = va_arg(ap, char*);
-				len = strlen(s);
-				if((precision >= 0) && (len > precision)) len = precision;
-				/* right justified : pad with spaces */
-				if(!(flags & LEFT)) while(len < wide--) *out++ = ' ';
-				for(i = 0; i < len; i++) *out++ = *s++;
-				/* left justified : pad with spaces */
-				while(len < wide--) *out++ = ' ';
-				break;
-
-			case 'c':
-				/* right justified : pad with spaces */
-				if(!(flags & LEFT)) while(1 < wide--) *out++ = ' ';
-				*out++ = (unsigned char)va_arg(ap, int32_t);
-				/* left justified : pad with spaces */
-				while(1 < wide--) *out++ = ' ';
-				break;
-			default:
-				return -1;
-		}
-	}
-	*out = 0;
-	return (int32_t)(out - start);
-}
-
-int sprintf(char *out, const char *fmt, ...) {
-  va_list args;
-	int32_t res;
-	va_start(args, fmt);
-	res = vsprintf(out, fmt, args);
-	va_end(args);
-	return res;
-}
-
-int snprintf(char *out, size_t n, const char *fmt, ...) {
   return 0;
 }
 
-int32_t get_wide(const char **s){
-	int32_t res = 0;
-	while(isdigit(**s)){
-		res = 10 * res + todigit(*(*s++));
+int sprintf(char *out, const char *fmt, ...) {
+  va_list sArgv;
+  int ret=0,val_d,len,tmp;
+  char ds[15];
+  char* val_s;
+  va_start(sArgv,fmt);
+  for (;;)
+  {
+	while (*fmt!='%'&&*fmt!='\0') *out=*fmt,++fmt,++out,++ret;
+	if (*fmt=='\0') {*out='\0';break;}
+	if (*(fmt+1)=='d')
+	{
+		val_d=va_arg(sArgv,int);
+		if (val_d<0) *out='-',++out,++ret;
+		len=0;
+		do
+		{
+			tmp=val_d%10;
+			ds[++len]=(tmp<0?-tmp:tmp)+'0';
+			val_d/=10;
+		}while (val_d!=0);
+		for (int i=len;i>=1;--i) *out=ds[i],++out;
+		ret+=len;
+		fmt=fmt+2;
 	}
-	return res;
+	else if (*(fmt+1)=='s')
+	{
+		val_s=va_arg(sArgv,char*);
+		while (*val_s!='\0') *out=*val_s,++val_s,++out,++ret;
+		fmt=fmt+2;
+	}
+  }
+  va_end(sArgv);
+  return ret==0?-1:ret;
 }
 
-#define LONG_STRSIZE_BASE_2 32
-void number_to_string(long num, int32_t base, int32_t flags, int32_t wide, int32_t precision, char **s)
-{
-	char sign; /* sign printed : '+', '-', ' ', or 0 (no sign) */
-	int32_t num_cpy = num;
-	unsigned long ul_num = (unsigned long)num; /* for unsigned format */
-
-	/* string representation of num (reversed) */
-	char tmp[LONG_STRSIZE_BASE_2];
-	int32_t i = 0; /* number of figures in tmp */
-
-	const char *digits = "0123456789ABCDEF";
-	if (flags & SMALL)
-		digits = "0123456789abcdef";
-
-	if ((base < 2) || (base > 16))
-		return;
-
-	if ((flags & SIGN) && (num < 0))
+int snprintf(char *out, size_t n, const char *fmt, ...) {
+  va_list sArgv;
+  int ret=0,val_d,len,tmp;
+  char ds[15];
+  char* val_s;
+  va_start(sArgv,fmt);
+  for (;;)
+  {
+	while (*fmt!='%'&&*fmt!='\0'&&n>0) *out=*fmt,++fmt,++out,++ret,--n;
+	if (n==0) break;
+	if (*fmt=='\0') {*out='\0';++ret;break;}
+	if (*(fmt+1)=='d')
 	{
-		sign = '-';
-		num = -num;
-	}
-	else
-		sign = (flags & PLUS) ? '+' : ((flags & SPACE) ? ' ' : 0);
-	if (sign)
-		wide--;
-
-	if (flags & SPECIAL)
-	{
-		if ((base == 16) && (num != 0))
-			wide -= 2; /* '0x' or '0X' */
-		if (base == 8)
+		val_d=va_arg(sArgv,int);
+		if (val_d<0) *out='-',++out,++ret,--n;
+		if (n==0) break;
+		len=0;
+		do
 		{
-			wide--;
-			precision--;
-		} /* '0' */
+			tmp=val_d%10;
+			ds[++len]=(tmp<0?-tmp:tmp)+'0';
+			val_d/=10;
+		}while (val_d!=0);
+		for (int i=len;n>0&&i>=1;--i) *out=ds[i],++out,--n,++ret;
+		++fmt;++fmt;
 	}
-
-	if (num == 0)
-		tmp[i++] = '0';
-	/* signed format */
-	if (flags & SIGN)
+	else if (*(fmt+1)=='s')
 	{
-		while (num != 0)
-		{
-			tmp[i++] = digits[num % base];
-			num = num / base;
-		}
+		val_s=va_arg(sArgv,char*);
+		while (*val_s!='\0'&&n>0) *out=*val_s,++val_s,++out,--n,++ret;
+		++fmt;++fmt;
 	}
-	/* unsigned format */
-	else
-	{
-		while (ul_num != 0)
-		{
-			tmp[i++] = digits[ul_num % base];
-			ul_num = ul_num / base;
-		}
-	}
-
-	if (i > precision)
-		precision = i;
-	wide -= precision;
-
-	/* wide = number of padding chars */
-	/* precision = number of figures after the sign and the special chars */
-
-	/* right justified and no zeropad : pad with spaces */
-	if (!(flags & (LEFT + ZERO)))
-		while (wide-- > 0)
-			*((*s)++) = ' ';
-
-	if (sign)
-		*((*s)++) = sign;
-	if ((flags & SPECIAL) && (num_cpy != 0))
-	{
-		if (base == 8)
-			*((*s)++) = '0';
-		if (base == 16)
-		{
-			*((*s)++) = '0';
-			if (flags & SMALL)
-				*((*s)++) = 'x';
-			else
-				*((*s)++) = 'X';
-		}
-	}
-
-	/* rigth justified and zeropad : pad with 0 */
-	if (!(flags & LEFT))
-		while (wide-- > 0)
-			*((*s)++) = '0';
-
-	/* print num */
-	while (i < precision--)
-		*((*s)++) = '0';
-	while (i-- > 0)
-		*((*s)++) = tmp[i];
-
-	/* left justfied : pad with spaces */
-	while (wide-- > 0)
-		*((*s)++) = ' ';
+  }
+  va_end(sArgv);
+  return ret==0?-1:ret;
 }
+
 #endif
-
-
-
