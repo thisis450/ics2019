cf82e2a5e21d29d8749833e90bb20b5af2488921
tracer-ics2019 <tracer@njuics.org>
2023-04-18 12:46:18 +0800

    >  run 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:46:18 up 1 day, 13:35,  1 user,  load average: 0.19, 0.10, 0.08 aeaac3458361536710c1278423fd49112dbf53

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,151 +3,143 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-	va_list sArgv;
-	int ret=0,val_d,len,tmp;
-	unsigned val_u=0;
-	char ds[35];
-	char* val_s;
-	va_start(sArgv,fmt);
-	for (;;)
-	{
-		while (*fmt!='%'&&*fmt!='\0') _putc(*fmt++),++ret;
-		if (*fmt=='\0') {_putc('\0');++ret;break;}
-		switch (*(fmt+1))
-		{
-			case 'd':
-			{
-				val_d=va_arg(sArgv,int);
-				if (val_d<0) _putc('-'),++ret;
-				len=0;
-				do
-				{
-					tmp=val_d%10;
-					ds[++len]=(tmp<0?-tmp:tmp)+'0';
-					val_d/=10;
-				}while(val_d);
-				for (int i=len;i>=1;--i) _putc(ds[i]);
-				ret+=len;
-				break;
-			}
-			case 's':
-			{
-				val_s=va_arg(sArgv,char*);
-				while (*val_s!='\0') _putc(*val_s),val_s++;
-				break;
-			}
-			case 'u':
-			{
-				val_u=va_arg(sArgv,unsigned);
-				len=0;
-				do
-				{
-					tmp=val_u%10;
-					ds[++len]=tmp+'0';
-					val_u/=10;
-				}while(val_u);
-				for (int i=len;i>=1;--i) _putc(ds[i]);
-				ret+=len;
-				break;
-			}
-			case 'x':
-			{
-				val_u=va_arg(sArgv,unsigned);
-				len=0;
-				do
-				{
-					tmp=val_u%16;
-					ds[++len]=(tmp<10?tmp+'0':tmp-9+'A'-1);
-					val_u/=16;
-				}while(val_u);
-				for (int i=len;i>=1;--i) _putc(ds[i]);
-				ret+=len;
-				break;
-			}
+struct param {
+	char uc:1;
+	char sign;			/* '-' 				 */
+	unsigned int base;	/* number base, e.g 8, 10, 16*/
+	char *bf;			/* buffer for output */
+};
+typedef void (*putcf) (void *, char);
 
-		}
-		fmt=fmt+2;
-	}
-	va_end(sArgv);
-	return ret==0?-1:ret;
+struct _vsprintf_putcf_data {
+	char *dest;
+	size_t num_chars;
+};
+
+static void _vsprintf_putcf(void *p, char c) {
+	struct _vsprintf_putcf_data *data = (struct _vsprintf_putcf_data *)p;
+	data->dest[data->num_chars ++] = c;
 }
 
+static void _printf_putc(void *p, char c) {  
+	_putc(c);
+}
+
+static void putchw(void *putp, putcf putf, struct param *p) {
+	char ch;
+	char *bf = p->bf;
+	while((ch = *bf++)) 
+		putf(putp, ch);
+}
+
+static void ui2a(unsigned int num, struct param *p) {
+	int n = 0;
+	unsigned int d = 1;
+	char *bf = p->bf;
+	while (num / d >= p->base)
+		d = d * p->base;
+
+	while (d != 0) {
+		int dgt = num /d;
+		num = num % d;
+		d = d / p->base;
+		if (n || dgt > 0 || d == 0) {
+			*bf++ = dgt + (dgt < 10 ? '0' : (p->uc ? 'A' : 'a') - 10);
+			++n;
+		}
+	}
+	*bf = 0;
+}
+static void i2a (int num, struct param *p) {
+	//printf("num = %d\n", num);
+	if (num < 0) {
+		num = -num;
+		p->sign = '-';
+	}
+	ui2a(num, p);
+}
+
+
+void my_format(void *putp, putcf putf, const char *fmt, va_list va) {
+	struct param p;
+	char bf[12];	/* int = 32b */
+	char ch;
+	p.bf = bf;
+
+	while ((ch = *(fmt++))) {
+		if (ch != '%') {
+			putf(putp, ch);
+		} else {
+			ch = *(fmt++);
+			switch (ch) {
+				case 'd':
+					p.base = 10;
+					p.uc = 0;
+					i2a(va_arg(va, int), &p);
+					putchw(putp, putf, &p);
+					break;
+				case 'x' :
+					p.base = 16;
+					p.uc = 0;
+					i2a(va_arg(va, int), &p);
+					putchw(putp, putf, &p);
+					break;
+				case 's':
+					p.bf = va_arg(va, char *);
+					putchw(putp, putf, &p);
+				    p.bf = bf; //???
+					break;
+				default : break;
+			}
+		}
+
+	}
+}
+
+
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
+	struct _vsprintf_putcf_data data;
+	data.dest = out;
+	data.num_chars = 0;
+	my_format(&data, _vsprintf_putcf, fmt, ap);
+	data.dest[data.num_chars] = '\0';
+
+	return data.num_chars;
+}
+static inline void _putstr(char *s) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  va_list sArgv;
-  int ret=0,val_d,len,tmp;
-  char ds[15];
-  char* val_s;
-  va_start(sArgv,fmt);
-  for (;;)
-  {
-	while (*fmt!='%'&&*fmt!='\0') *out=*fmt,++fmt,++out,++ret;
-	if (*fmt=='\0') {*out='\0';break;}
-	if (*(fmt+1)=='d')
-	{
-		val_d=va_arg(sArgv,int);
-		if (val_d<0) *out='-',++out,++ret;
-		len=0;
-		do
-		{
-			tmp=val_d%10;
-			ds[++len]=(tmp<0?-tmp:tmp)+'0';
-			val_d/=10;
-		}while (val_d!=0);
-		for (int i=len;i>=1;--i) *out=ds[i],++out;
-		ret+=len;
-		fmt=fmt+2;
-	}
-	else if (*(fmt+1)=='s')
-	{
-		val_s=va_arg(sArgv,char*);
-		while (*val_s!='\0') *out=*val_s,++val_s,++out,++ret;
-		fmt=fmt+2;
-	}
-  }
-  va_end(sArgv);
-  return ret==0?-1:ret;
+	va_list ap;
+	int ret;
+	
+	va_start(ap, fmt);
+	ret = vsprintf(out, fmt, ap);
+	va_end(ap);
+
+	return ret;
 }
 
+int printf(const char *fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+	my_format(NULL, _printf_putc, fmt, ap);
+	va_end(ap);
+
+	/*
+		0x3F8 
+		printf -> _putc(ch) -> putchar(0x3F8, ch) -> outb(addr, ch) -> *addr = ch
+	 */
+	return 0;
+}
+/* snprintf()用于将格式化的数据写入字符串 
+ * out: 为要写入的字符串
+ * n  : 要写入的字符的最大数据，超过n会被截断
+ * fmt: 格式化字符串
+ */
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  va_list sArgv;
-  int ret=0,val_d,len,tmp;
-  char ds[15];
-  char* val_s;
-  va_start(sArgv,fmt);
-  for (;;)
-  {
-	while (*fmt!='%'&&*fmt!='\0'&&n>0) *out=*fmt,++fmt,++out,++ret,--n;
-	if (n==0) break;
-	if (*fmt=='\0') {*out='\0';++ret;break;}
-	if (*(fmt+1)=='d')
-	{
-		val_d=va_arg(sArgv,int);
-		if (val_d<0) *out='-',++out,++ret,--n;
-		if (n==0) break;
-		len=0;
-		do
-		{
-			tmp=val_d%10;
-			ds[++len]=(tmp<0?-tmp:tmp)+'0';
-			val_d/=10;
-		}while (val_d!=0);
-		for (int i=len;n>0&&i>=1;--i) *out=ds[i],++out,--n,++ret;
-		++fmt;++fmt;
-	}
-	else if (*(fmt+1)=='s')
-	{
-		val_s=va_arg(sArgv,char*);
-		while (*val_s!='\0'&&n>0) *out=*val_s,++val_s,++out,--n,++ret;
-		++fmt;++fmt;
-	}
-  }
-  va_end(sArgv);
-  return ret==0?-1:ret;
+
+  return 0;
 }
 
 #endif
