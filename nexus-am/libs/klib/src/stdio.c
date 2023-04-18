ca87dc89ddd76276d6005042fdeec389bf5a5fdb
tracer-ics2019 <tracer@njuics.org>
2023-04-18 12:58:34 +0800

    >  run 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:58:33 up 1 day, 13:47,  1 user,  load average: 0.50, 0.40, 0.23 3755edb979be8ba340d121b8f6628e6e245612f1

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,145 +3,143 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+struct param {
+	char uc:1;
+	char sign;			/* '-' 				 */
+	unsigned int base;	/* number base, e.g 8, 10, 16*/
+	char *bf;			/* buffer for output */
+};
+typedef void (*putcf) (void *, char);
 
-static char int_buf[20]; // 最大的long long十进制长度为19
-char* hex_digits = "0123456789abcdef";
-// base表示是几进制, 返回长度
-static int fill_int_buf(unsigned long long lval, unsigned base) {
-  int cur_pos = 0;
-  if(lval==0) {
-    int_buf[0]='0';
-    int_buf[1]='\0';
-    return 1;
-  }
-  char tmp;
-  while (lval > 0) {
-    int_buf[cur_pos++] = hex_digits[(lval % base)]; // 由于10进制的digits与hex的digits相同, 所以用hex_digits即可
-    lval /= base;
-  }
-  int left = 0;
-  int right = cur_pos - 1; // 现在cur_pos是右开, 因此-1
-  while (left < right) {
-    tmp = int_buf[right];
-    int_buf[right] = int_buf[left];
-    int_buf[left] = tmp;
-    ++left;
-    --right;
-  }
-  int_buf[cur_pos]='\0';
-  return cur_pos;
+struct _vsprintf_putcf_data {
+	char *dest;
+	size_t num_chars;
+};
+
+static void _vsprintf_putcf(void *p, char c) {
+	struct _vsprintf_putcf_data *data = (struct _vsprintf_putcf_data *)p;
+	data->dest[data->num_chars ++] = c;
 }
 
-int allprintf(char *out, size_t size, const char *fmt, va_list ap) {
-#define out_putc(out_putc_ch) if(out!=NULL) {\
-          out[cur_pos++] = out_putc_ch;\
-      } else {\
-        cur_pos++;\
-        _putc(out_putc_ch);\
-      }
-#define check_out_putc(out_putc_ch) if(cur_pos==size_minus_one) {\
-goto return_label;\
-} else {out_putc(out_putc_ch);}
-  assert(size>0);
-  size_t size_minus_one = size-1;
-  size_t cur_pos = 0; // cur_pos指向out, j指向fmt
-  const char* p;
-  int ival;
-  unsigned long long lval = 0; // 为了过编
-  size_t int_buf_len;
-  char tmp_ch;
-  
-  for (p = fmt; *p; ++p) {
-    if (*p != '%') {
-      check_out_putc(*p);
-      continue;
-    }
+static void _printf_putc(void *p, char c) {  
+	_putc(c);
+}
 
-    // 处理其它情况
-    ++p;
-    switch (*p) {
-    case 'c': {
-      char ch = va_arg(ap, int);
-      check_out_putc(ch);
-      break;
-    }
-    case 's': {
-      for (char* s = va_arg(ap, char*); *s; ++s) {
-        check_out_putc(*s);
-      }
-      break;
-    }
-    case 'd': {
-      ival = va_arg(ap, int);
-      if (ival < 0) {
-        check_out_putc('-');
-        lval = (-ival);
-      } else {
-        lval = ival;
-      }
-      int_buf_len = fill_int_buf(lval, 10);
-      for(size_t i = 0; i<int_buf_len; i++) {
-        tmp_ch = int_buf[i];
-        check_out_putc(tmp_ch);
-      }
-      break;
-    }
-    case 'u': {
-      lval = va_arg(ap, unsigned int);
-      int_buf_len = fill_int_buf(lval, 10);
-      for(size_t i = 0; i<int_buf_len; i++) {
-        tmp_ch = int_buf[i];
-        check_out_putc(tmp_ch);
-      }
-      break;
-    }
-    case 'x': {
-      lval = va_arg(ap, unsigned int);
-      int_buf_len = fill_int_buf(lval, 16);
-      for(size_t i = 0; i<int_buf_len; i++) {
-        tmp_ch = int_buf[i];
-        check_out_putc(tmp_ch);
-      }
-      break;
-    }
-    default: {
-      return -1;
-    }
-    }
-  }
-return_label:
-  out_putc('\0');
-  return (cur_pos-1); // 因为cur_pos包括\0
-#undef out_putc
-#undef check_out_putc
+static void putchw(void *putp, putcf putf, struct param *p) {
+	char ch;
+	char *bf = p->bf;
+	while((ch = *bf++)) 
+		putf(putp, ch);
+}
+
+static void ui2a(unsigned int num, struct param *p) {
+	int n = 0;
+	unsigned int d = 1;
+	char *bf = p->bf;
+	while (num / d >= p->base)
+		d = d * p->base;
+
+	while (d != 0) {
+		int dgt = num /d;
+		num = num % d;
+		d = d / p->base;
+		if (n || dgt > 0 || d == 0) {
+			*bf++ = dgt + (dgt < 10 ? '0' : (p->uc ? 'A' : 'a') - 10);
+			++n;
+		}
+	}
+	*bf = 0;
+}
+static void i2a (int num, struct param *p) {
+	//printf("num = %d\n", num);
+	if (num < 0) {
+		num = -num;
+		p->sign = '-';
+	}
+	ui2a(num, p);
+}
+
+
+void my_format(void *putp, putcf putf, const char *fmt, va_list va) {
+	struct param p;
+	char bf[12];	/* int = 32b */
+	char ch;
+	p.bf = bf;
+
+	while ((ch = *(fmt++))) {
+		if (ch != '%') {
+			putf(putp, ch);
+		} else {
+			ch = *(fmt++);
+			switch (ch) {
+				case 'd':
+					p.base = 10;
+					p.uc = 0;
+					i2a(va_arg(va, int), &p);
+					putchw(putp, putf, &p);
+					break;
+				case 'x' :
+					p.base = 16;
+					p.uc = 0;
+					i2a(va_arg(va, int), &p);
+					putchw(putp, putf, &p);
+					break;
+				case 's':
+					p.bf = va_arg(va, char *);
+					putchw(putp, putf, &p);
+				    p.bf = bf; //???
+					break;
+				default : break;
+			}
+		}
+
+	}
+}
+
+
+int vsprintf(char *out, const char *fmt, va_list ap) {
+	struct _vsprintf_putcf_data data;
+	data.dest = out;
+	data.num_chars = 0;
+	my_format(&data, _vsprintf_putcf, fmt, ap);
+	data.dest[data.num_chars] = '\0';
+
+	return data.num_chars;
+}
+static inline void _putstr(char *s) {
+}
+
+int sprintf(char *out, const char *fmt, ...) {
+	va_list ap;
+	int ret;
+	
+	va_start(ap, fmt);
+	ret = vsprintf(out, fmt, ap);
+	va_end(ap);
+
+	return ret;
 }
 
 int printf(const char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  allprintf(NULL, 0xffffffffu, fmt, ap);
-  va_end(ap);
-  return 0;
-}
+	va_list ap;
+	va_start(ap, fmt);
+	my_format(NULL, _printf_putc, fmt, ap);
+	va_end(ap);
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
+	/*
+		0x3F8 
+		printf -> _putc(ch) -> putchar(0x3F8, ch) -> outb(addr, ch) -> *addr = ch
+	 */
+	return 0;
 }
-
-int sprintf(char* out, const char* fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  int ret = allprintf(out, 0xffffffffu, fmt, ap);
-  va_end(ap);
-  return ret;
-}
-
+/* snprintf()用于将格式化的数据写入字符串 
+ * out: 为要写入的字符串
+ * n  : 要写入的字符的最大数据，超过n会被截断
+ * fmt: 格式化字符串
+ */
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  int ret = allprintf(out, n, fmt, ap);
-  va_end(ap);
-  return ret;
+
+  return 0;
 }
 
 #endif
