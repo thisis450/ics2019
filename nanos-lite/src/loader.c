e66aa4085ead3d870eaaf2021e96514e616026e8
tracer-ics2019 <tracer@njuics.org>
2023-05-08 01:19:58 +0800

    >  compile 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:19:58 up 16 days,  5:02,  4 users,  load average: 0.16, 0.03, 0.01 e77273eaed69913267a11bf164bef45df87c0b63

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -10,34 +10,8 @@
 #endif
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  //TODO();
-	int fd = fs_open(filename, 0, 0);
-	Elf_Ehdr elf_ehdr;
-	fs_lseek(fd, 0, SEEK_SET);
-	fs_read(fd, (void *)&elf_ehdr, sizeof(Elf_Ehdr));
-	if (elf_ehdr.e_ident[EI_MAG0] != ELFMAG0 ||
-			elf_ehdr.e_ident[EI_MAG1] != ELFMAG1 ||
-			elf_ehdr.e_ident[EI_MAG2] != ELFMAG2 ||
-			elf_ehdr.e_ident[EI_MAG3] != ELFMAG3){
-		Log("ELF format error in ramdisk\n");
-		assert(0);
-	}
-	Elf_Phdr elf_phdr;
-	for (uint16_t i = 0; i < elf_ehdr.e_phnum; i++){
-		fs_lseek(fd, elf_ehdr.e_phoff + i * elf_ehdr.e_phentsize, SEEK_SET);
-		fs_read(fd, (void *)&elf_phdr, sizeof(Elf_Phdr));
-		Log("segment p_offset from phdr: 0x%x, from ehdr: 0x%x\n", elf_phdr.p_offset, elf_ehdr.e_phoff + i * elf_ehdr.e_phentsize);
-		Log("current type: %d\n", elf_phdr.p_type);
-		if (elf_phdr.p_type != PT_LOAD){
-			continue;
-		}
-		fs_lseek(fd, elf_phdr.p_offset, SEEK_SET);
-		fs_read(fd, (void *)elf_phdr.p_vaddr, elf_phdr.p_filesz);
-		if (elf_phdr.p_memsz > elf_phdr.p_filesz){
-			memset((void *)elf_phdr.p_vaddr + elf_phdr.p_filesz, 0, elf_phdr.p_memsz - elf_phdr.p_filesz);
-		}
-	}
-	return elf_ehdr.e_entry;
+  TODO();
+  return 0;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
