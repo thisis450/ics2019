2300bf9e07d452b41e551efb2a67b3bf115a231b
tracer-ics2019 <tracer@njuics.org>
2023-05-08 02:09:45 +0800

    >  compile 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:09:45 up 16 days,  5:52,  4 users,  load average: 0.03, 0.03, 0.00 b251c687e4842cd6e6a796fcf53da80a1a92e755

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -6,16 +6,16 @@ void raise_intr(uint32_t NO, vaddr_t ret_addr) {
    */
 	/* 1. push eflags, cs, eip */
 	// printf("syscall NO: 0x%x and ret-addr: 0x%x\n", NO, ret_addr);
-	rtl_push(&cpu.eflags.eflags_32);
+	rtl_push(&cpu.eflags.val);
 	rtl_push(&cpu.cs);
-	rtl_push(&ret	_addr);
+	rtl_push(&ret_addr);
 
 	/* 2 3. get IDT base addr and entry from IDTR */
-	vaddr_t idt_entry = cpu.idtr.base + NO * IDT_ENTRY_SZ;
+	vaddr_t idt_entry = cpu.idtr.base + NO * 8;
 	// printf("IDT entry address: 0x%x\n", idt_entry);
 
 	/* 4. generate exception entry addr */
-	vaddr_t ent_hi = vaddr_read(idt_entry + IDT_ENTRY_HI_OFF, 4) & 0xffff0000;
+	vaddr_t ent_hi = vaddr_read(idt_entry + 4, 4) & 0xffff0000;
 	vaddr_t ent_lo = vaddr_read(idt_entry, 2) & 0xffff;
 	// printf("Handler target address: 0x%x\n", ent_hi | ent_lo);
 
