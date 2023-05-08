3dc26e089596525832a156f4ef5e87af6d4651c9
tracer-ics2019 <tracer@njuics.org>
2023-05-08 11:05:15 +0800

    >  run 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:05:15 up 16 days, 14:48,  4 users,  load average: 0.04, 0.06, 0.02 5e251345561214eceeb8f2645a9de8c82425d3a

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,6 +2,12 @@
 
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
+		case _EVENT_YIELD:
+			 Log("_EVENT_YIELD num 0x%x triggered\n", c->irq);
+			break;
+		case _EVENT_SYSCALL:
+			return do_syscall(c);
+			break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
