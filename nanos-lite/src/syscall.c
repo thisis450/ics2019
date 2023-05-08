4ca6a0fc12ffc5b402d5d9992cefe0537a62aacb
2012159-Jinleqing <1185056784@qq.com>
2023-05-09 01:45:28 +0800

    On branch pa3  Changes to be committed: 	modified:   nanos-lite/src/syscall.c 	modified:   nexus-am/am/include/arch/x86-nemu.h

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,10 +4,14 @@
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-
+  a[1] = c->GPR2;
+	a[2] = c->GPR3;
+	a[3] = c->GPR4;
+uintptr_t res = 0;
   switch (a[0]) {
+    case SYS_yield: res = sys_yield(); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
-
+c->GPRx = res;
   return NULL;
 }
