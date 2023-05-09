f80f848788efd1f3dec7082c265250b9b4d1eb20
tracer-ics2019 <tracer@njuics.org>
2023-05-10 02:02:33 +0800

    >  run 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:02:33 up 18 days,  5:45,  4 users,  load average: 0.39, 0.20, 0.09 2c1d8491535115366eca5f28060953053223ad4

--- nexus-am/am/src/x86/nemu/cte.c
+++ nexus-am/am/src/x86/nemu/cte.c
@@ -16,6 +16,9 @@ _Context* __am_irq_handle(_Context *c) {
       case 0x81:
 				ev.event = _EVENT_YIELD;
 			break;
+      case 0x80:
+				ev.event = _EVENT_SYSCALL;
+				break;
       default: ev.event = _EVENT_ERROR; break;
     }
     printf("irq=%d\n", c->irq);
