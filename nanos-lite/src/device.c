f77908eece8356e064bc7b44a1777d9710fc9bf7
tracer-ics2019 <tracer@njuics.org>
2023-05-10 02:20:39 +0800

    >  run 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:20:39 up 18 days,  6:03,  4 users,  load average: 0.00, 0.04, 0.07 a1a4445615d6ac5b44fcff65e99bac4fb7ba9c94

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,12 +1,8 @@
 #include "common.h"
 #include <amdev.h>
-#define KEYDOWN_MASK 0x8000
+
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  char *p_buf = (char *)buf;
-	for(size_t i = 0; i < len; i++){
-		_putc(p_buf[i]);
-	}
-	return len;
+  return 0;
 }
 
 #define NAME(key) \
@@ -18,48 +14,21 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  int keycode = read_key();
-	if(keycode != _KEY_NONE){
-		if(keycode & KEYDOWN_MASK)
-    {
-			keycode = keycode ^ KEYDOWN_MASK;
-			sprintf(buf, "kd %s\n", keyname[keycode]);
-		}
-		else{
-			sprintf(buf, "ku %s\n", keyname[keycode]);
-		}
-	}
-	else{
-		sprintf(buf, "t %u\n", uptime());
-	}
-	return strlen(buf);
+  return 0;
 }
 
 static char dispinfo[128] __attribute__((used)) = {};
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-	printf("[displayinfo_read] try to read offset: %lu, and len: %lu.\n", offset, len);
-  strncpy((char *)buf, dispinfo + offset, len);
-	return len;
+  return 0;
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-	printf("[fb_write] try to write offset: %lu, and size: %lu.\n", offset, len);
-	/* write the len bytes from buf to the screen(offset -> coord) */
-	/* calc w from len */
-	int w = len / 4;
-	int h = 1;
-	/* convert to int unit offset */
-	offset = offset / 4;
-	int x = offset % screen_width();
-	int y = offset / screen_width();
-	draw_rect((uint32_t *)buf, x, y, w, h);
-	return len;
+  return 0;
 }
 
 size_t fbsync_write(const void *buf, size_t offset, size_t len) {
-  draw_sync();
-	return 0;
+  return 0;
 }
 
 void init_device() {
@@ -68,7 +37,4 @@ void init_device() {
 
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
-  	memset(dispinfo, 0, 128 * sizeof(char));
-	sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d\n", screen_width(), screen_height());
-	printf("dispinfo:\n%s\n", dispinfo);
 }
