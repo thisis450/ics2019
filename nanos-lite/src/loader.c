60aba57be45702cb494f29244bf68f2f0e8e8c3c
tracer-ics2019 <tracer@njuics.org>
2023-05-10 09:45:03 +0800

    >  run 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:45:03 up 18 days, 13:27,  4 users,  load average: 0.22, 0.17, 0.07 f718d8d9bae3ce89dd13baf9eeee29cebe7ac38

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,6 +1,5 @@
 #include "proc.h"
 #include <elf.h>
-#define DEFAULT_ENTRY ((void *)0x4000000)
 #ifdef __ISA_AM_NATIVE__
 # define Elf_Ehdr Elf64_Ehdr
 # define Elf_Phdr Elf64_Phdr
@@ -9,12 +8,41 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
-static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-	// Elf_Ehdr elf_ehdr;
-	// /* read elf-header from ramdisk */
-	// ramdisk_read((void *)&elf_ehdr, 0, sizeof(Elf_Ehdr));
-  //  return elf_ehdr.e_entry;
+static uintptr_t loader(PCB *pcb, const char *filename)
+{
+  //TODO();
+  // int fd = fs_open(filename, 0, 0);
+  unsigned char buf[2048];
+  Elf_Ehdr elf_head;
+  // fs_read(fd, &elf_head, sizeof(Elf_Ehdr));
+  ramdisk_read(&elf_head, 0, sizeof(Elf_Ehdr));
+  Elf_Phdr elf_phentry;
+  for (int i = elf_head.e_phoff; i < elf_head.e_phnum * elf_head.e_phentsize + elf_head.e_phoff; i += elf_head.e_phentsize)
+  {
+    // fs_lseek(fd, elf_head.e_phoff + i * elf_head.e_phentsize, SEEK_SET);
+    // fs_read(fd, &elf_phentry, elf_head.e_phentsize);
+    ramdisk_read(&elf_phentry, i, sizeof(Elf_Phdr));
+    if (elf_phentry.p_type == PT_LOAD)
+    {
+      int len = 0;
+      size_t file_off = elf_phentry.p_offset;
+      unsigned char *mem_addr = (unsigned char *)elf_phentry.p_vaddr;
+      while (len < elf_phentry.p_filesz)
+      {
+        int mov_size = (elf_phentry.p_filesz - len > 2048 ? 2048 : elf_phentry.p_filesz - len);
+        // fs_lseek(fd, file_off, SEEK_SET);
+        // fs_read(fd, buf, mov_size);
+        ramdisk_read(buf, file_off, mov_size);
+        memcpy(mem_addr, buf, mov_size);
+        file_off += mov_size;
+        mem_addr += mov_size;
+        len += mov_size;
+      }
+      memset(mem_addr, 0, elf_phentry.p_memsz - len);
+    }
+  }
+  // fs_close(fd);
+  return elf_head.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
