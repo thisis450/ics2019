1f4b48e75e210c04f61b39665a569a3a966a85ba
tracer-ics2019 <tracer@njuics.org>
2023-05-10 09:53:25 +0800

    >  run 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:53:25 up 18 days, 13:36,  4 users,  load average: 0.11, 0.12, 0.08 27e34fab23626c09da456796cf2529718d96c86a

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -8,6 +8,19 @@ uint32_t sys_yield(){
 uint32_t sys_exit(int state){
 	_halt(state);
 }
+uint32_t sys_write(int fd, const void *buf, size_t count){
+	/* only write to stdout or stderr,
+	 * for didn't implement file system yet.
+	 */
+	Log("called write!!\n");
+	if(fd == 1 || fd == 2){
+		size_t i;
+		for(i = 0; i < count; i++){
+			_putc(((char *)buf)[i]);
+		}
+	}
+	return count;
+}
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -18,6 +31,7 @@ uintptr_t res = 0;
   switch (a[0]) {
 		case SYS_yield: Log("sys_exit \n");res = sys_yield(); break;
 		case SYS_exit: res = sys_exit(a[1]); break;
+    case SYS_write: res = sys_write(a[1], (void *)a[2], a[3]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 c->GPRx = res;
