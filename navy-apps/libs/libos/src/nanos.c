a824ef3ba191f8693700bd2600eec13cccf32d86
tracer-ics2019 <tracer@njuics.org>
2023-05-12 01:18:54 +0800

    >  run 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:18:54 up  1:39,  1 user,  load average: 0.02, 0.05, 0.01 a1b42078ed160c426ce6ce9777c35edc13dff03

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -77,18 +77,16 @@ void *_sbrk(intptr_t increment) {
 }
 
 int _read(int fd, void *buf, size_t count) {
-  _exit(SYS_read);
-  return 0;
+return _syscall_(SYS_read, fd, (intptr_t)buf, (intptr_t)count);
+
 }
 
 int _close(int fd) {
-  _exit(SYS_close);
-  return 0;
+return _syscall_(SYS_close, fd, 0, 0);
 }
 
 off_t _lseek(int fd, off_t offset, int whence) {
-  _exit(SYS_lseek);
-  return 0;
+return _syscall_(SYS_lseek, fd, offset, whence);
 }
 
 int _execve(const char *fname, char * const argv[], char *const envp[]) {
