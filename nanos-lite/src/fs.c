583a2379217c0c7e260a7d1c5a3f90f6d5eb2c34
tracer-ics2019 <tracer@njuics.org>
2023-05-11 23:12:52 +0800

    >  run 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:12:52 up 20 days,  2:55,  4 users,  load average: 0.13, 0.05, 0.01 584ba95dc9e33e61ea289bd66f7b495cd07d1279

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -83,6 +83,7 @@ int fs_open(const char *filename,int flags,int mode)
 size_t fs_read(int fd,void*buf,size_t len)
 {
   assert(fd>=0&&fd<NR_FILES);
+  Log("fs_read:try to open fd%d",fd);
   if(fd<3)
   {
     Log("fs_read:try to open fd%d,system file",fd);
