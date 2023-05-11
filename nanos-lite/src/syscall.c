9821dec0ff65cf2e28ce33f927a6759d8b6abb77
tracer-ics2019 <tracer@njuics.org>
2023-05-11 23:17:57 +0800

    >  run 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:17:57 up 20 days,  3:00,  4 users,  load average: 0.14, 0.09, 0.02 b7635b347e8efc1e87b0c7689e28bf9592927228

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -12,19 +12,7 @@ uint32_t sys_brk(int addr)
 {
 	return 0;
 }
-uint32_t sys_read(int fd, void *buf, size_t len){
-	return fs_read(fd, buf, len);
-}
-uint32_t sys_open(const char *pathname, int flags){
-	/* return the file descriptor */
-	return fs_open(pathname, 0, 0);
-}
-uint32_t sys_lseek(int fd, size_t offset, int whence){
-	return fs_lseek(fd, offset, whence);
-}
-uint32_t sys_close(int fd){
-	return fs_close(fd);
-}
+
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -36,11 +24,11 @@ uintptr_t res = 0;
 		case SYS_yield: Log("sys_exit \n");res = sys_yield(); break;
 		case SYS_exit: res = sys_exit(a[1]); break;
 		case SYS_brk: res=sys_brk(a[1]); break;
-		case SYS_open: res = sys_open((const char *)a[1], a[2]); break;
-		case SYS_read: res = sys_read(a[1], (void *)a[2], a[3]); break;
+		case SYS_open: res = fs_open((const char *)a[1], a[2]); break;
+		case SYS_read: res = fs_read(a[1], (void *)a[2], a[3]); break;
 		case SYS_write: res = fs_write((int)a[1],(const void *)a[2],(size_t)a[3]); break;
-		case SYS_close: res = sys_close(a[1]); break;
-		case SYS_lseek: res = sys_lseek(a[1], a[2], a[3]); break;
+		case SYS_close: res = fs_close(a[1]); break;
+		case SYS_lseek: res = fs_lseek(a[1], a[2], a[3]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 c->GPRx = res;
