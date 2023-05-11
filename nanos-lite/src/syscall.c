38fd8fa53d3005105e69a18c5367589d843a6945
tracer-ics2019 <tracer@njuics.org>
2023-05-12 01:27:03 +0800

    >  run 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:27:03 up  1:47,  1 user,  load average: 0.09, 0.05, 0.01 1d1881f9bc21007772da326229180a61b37e2ee4

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -12,7 +12,20 @@ uint32_t sys_brk(int addr)
 {
 	return 0;
 }
-
+uint32_t sys_write(int fd, const void *buf, size_t count){
+	/* only write to stdout or stderr,
+	 * for didn't implement file system yet.
+	 */
+	Log("called write!!\n");
+	if(fd == 1 || fd == 2){
+		size_t i;
+		for(i = 0; i < count; i++){
+			_putc(((char *)buf)[i]);
+		}
+	}
+	return count;
+	return fs_write(fd, buf, count);
+}
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -26,7 +39,7 @@ uintptr_t res = 0;
 		case SYS_brk:Log("sys_brk \n"); res=sys_brk(a[1]); break;
 		case SYS_open: Log("sys_open \n");res = fs_open((const char *)a[1], a[2]); break;
 		case SYS_read: Log("sys_read \n");res = fs_read(a[1], (void *)a[2], a[3]); break;
-		case SYS_write: Log("sys_write \n");res = fs_write((int)a[1],(const void *)a[2],(size_t)a[3]); break;
+		case SYS_write: Log("sys_write \n");res = sys_write((int)a[1],(const void *)a[2],(size_t)a[3]); break;
 		case SYS_close: Log("sys_close \n");res = fs_close(a[1]); break;
 		case SYS_lseek: Log("sys_lseek \n");res = fs_lseek(a[1], a[2], a[3]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
