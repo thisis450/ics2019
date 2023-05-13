40c7efb95319b7debdcb4710639c8a755edef33a
tracer-ics2019 <tracer@njuics.org>
2023-05-13 21:21:24 +0800

    >  run 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:21:24 up  1:55,  1 user,  load average: 0.09, 0.14, 0.22 5885793d3b98b2b54b58e115dcfd71952c22d97b

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -88,36 +88,40 @@ int fs_open(const char *filename,int flags,int mode)
 	panic("fs_open:no such file named %s",filename);
 	return -1;
 }
-size_t fs_read(int fd,void*buf,size_t len)
-{
-  assert(fd>=0&&fd<NR_FILES);
-  //Log("fs_read:try to open fd%d",fd);
-  if(fd<3||fd==FD_FB)
-  {
-    //Log("fs_read:try to open fd%d,system file",fd);
-    return 0;
-  }
-    if(fd==FD_EVENTS)
-  {
-    return events_read(buf,0,len);
-  }
-    int n=fs_filesize(fd)-open_offset(fd);
-    if(len>n)
-  {
-    len=n;
-  }
- 
-   if(fd==FD_DISPINFO)
-   {
-    dispinfo_read(buf,open_offset(fd),len);
-    set_open_offset(fd,open_offset(fd)+len);
-    return len;
-   }
+size_t fs_read(int fd, void *buf, size_t len){
+	assert(fd >= 0 && fd < NR_FILES);
+	/* operate reading via ramdisk APIs */
+	Finfo file = file_table[fd];
+	size_t rlen = -1;
 
-  
-  ramdisk_read(buf,disk_offset(fd)+open_offset(fd),len);
-  set_open_offset(fd,open_offset(fd)+len);
-  return len;
+	/* set the correct read size */
+	if (file.open_offset > file.size)
+	{
+		Log("[fs_read failed] file %d current open offset %d > file size %d.\n", fd, file.open_offset, file.size);
+		return 0; //EOF
+	}
+
+	if (file.read){
+		rlen = file.read(buf, file.open_offset, len);
+		(&file_table[fd])->open_offset += rlen;
+	}
+	else{
+		/* set the correct read size */
+		/* set correct base offset */
+		size_t base_offset = file.open_offset < 0 ? 0 : file.open_offset;
+		len = file.open_offset < 0 ? len + file.open_offset : len;
+		if (len < 0){
+			return 0;
+		}
+		if(base_offset + len > file.size){
+			len = file.size - base_offset;
+		}
+		/* read via ramdisk API */
+		rlen = ramdisk_read(buf, file.disk_offset + base_offset, len);
+		/* update open offset */
+		(&file_table[fd])->open_offset = base_offset + rlen;
+	}
+	return rlen;
 }
 int fs_close(int fd)
 {
