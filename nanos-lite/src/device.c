d2f5f026648f025589b0190a69f45f8e90218f7a
tracer-ics2019 <tracer@njuics.org>
2023-05-13 18:08:52 +0800

    >  run 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:08:52 up 1 day, 18:29,  1 user,  load average: 0.05, 0.15, 0.16 f3775b14560e771415445651de55eb052922

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -17,8 +17,9 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t offset, size_t len) {
+	sprintf(buf,"events_read\n");
   int key = read_key();
-  sprintf(buf,"events_read\n");
+  
 	if(key != _KEY_NONE)
   {
 		if(key & 0x8000)
