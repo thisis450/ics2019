715e99c56bd2f027683d416806ae9e2e3ddb9091
tracer-ics2019 <tracer@njuics.org>
2023-05-13 21:46:16 +0800

    >  run 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:46:16 up  2:20,  1 user,  load average: 0.32, 0.23, 0.24 4c88d665359e007e9c9d751ff88e1c41e7274c36

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -80,9 +80,7 @@ size_t fs_read(int fd, void *buf, size_t len){
 		if(file_table[fd].open_offset + len > file_table[fd].size){
 			len = file_table[fd].size - file_table[fd].open_offset;
 		}
-		/* read via ramdisk API */
 		rlen = ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
-		/* update open offset */
 		(&file_table[fd])->open_offset = file_table[fd].open_offset + rlen;
 	}
 	return rlen;
@@ -92,29 +90,23 @@ size_t fs_write(int fd, const void *buf, size_t len)
 {
 	assert(fd >= 0 && fd < NR_FILES);
 	size_t wlen = -1;
-	Finfo file = file_table[fd];
-	if (file.open_offset > file.size){
-		printf("[fs_write failed] file %d current open offset %lu> file size %lu.\n", fd, file.open_offset, file.size);
+	if (file_table[fd].open_offset > file_table[fd].size){
+		Log("fs_write out of size\n");
 		return 0;
 	}
-	if(file.write){
-		wlen = file.write(buf, file.open_offset, len);
+	if(file_table[fd].write){
+		wlen = file_table[fd].write(buf, file_table[fd].open_offset, len);
 		(&file_table[fd])->open_offset += wlen;
 	}
 	else{
-		/* set correct base offset */
-		size_t base_offset = file.open_offset < 0 ? 0 : file.open_offset;
-		len = file.open_offset < 0 ? len + file.open_offset : len;
-		if(len < 0){
-			return 0;
-		}
-		if(base_offset + len > file.size){
-			len = file.size - base_offset;
+
+		if(file_table[fd].open_offset + len > file_table[fd].size){
+			len = file_table[fd].size - file_table[fd].open_offset;
 		}
 		/* write via ramdisk API */
-		wlen = ramdisk_write(buf, file.disk_offset + base_offset, len);
+		wlen = ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
 		/* update open offset */
-		(&file_table[fd])->open_offset = base_offset + wlen;
+		(&file_table[fd])->open_offset = file_table[fd].open_offset + wlen;
 	}
 	return wlen;
 }
