1c81047711abfdb5ae393cdaf37aa30a6d2b7ee9
tracer-ics2019 <tracer@njuics.org>
2023-06-01 07:46:53 +0800

    >  run 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:46:53 up 16 days,  7:50,  1 user,  load average: 0.31, 0.15, 0.07 93af7f6a197cee2144415821c25408dcc54f7f3

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -13,6 +13,7 @@ void switch_boot_pcb() {
 void hello_fun(void *arg) {
   int j = 1;
   while (1) {
+    printf("arg is %s",arg);
     Log("Hello World from Nanos-lite for the %dth time!,arg is %s", j,arg);
     j ++;
     _yield();
