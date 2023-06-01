5d25b3b96bafba265a20122faf44f0288063b5eb
tracer-ics2019 <tracer@njuics.org>
2023-06-01 13:42:56 +0800

    >  compile 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:42:56 up 16 days, 13:46,  1 user,  load average: 0.11, 0.03, 0.01 a37924a79ae9e4cc3409afc3aa1320db2a8de3f

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -1,68 +1,70 @@
 #include "nemu.h"
 
-#define PTXSHFT 12 // Offset of PTX in a linear address
-#define PDXSHFT 22 // Offset of PDX in a linear address
-#define PDX(va) (((uint32_t)(va) >> PDXSHFT) & 0x3ff)
-#define PTX(va) (((uint32_t)(va) >> PTXSHFT) & 0x3ff)
-#define OFF(va) ((uint32_t)(va)&0xfff)
-#define VPAGE_NUM(va) ((uint32_t)(va) >> PTXSHFT)
-#define PTE_ADDR(pte) ((uint32_t)(pte) & ~0xfff)
-
-paddr_t page_translate(vaddr_t addr)
+uint32_t page_translate(uint32_t addr)
 {
-  uint32_t dir_index = PDX(addr);
-  uint32_t tab_index = PTX(addr);
-  paddr_t pgdir = cpu.cr3.val;
-  PDE pde;
-  pde.val = paddr_read(pgdir + dir_index * 4, 4);
-  if (!pde.present) {
-    Log("%x %x", addr, pde.val);
-    assert(0);
-  }
-  PTE pte;
-  pte.val = paddr_read((pde.val & ~0xfff) + tab_index * 4, 4);
-  if (!pte.present)
-    assert(0);
-  return (PTE_ADDR(pte.val) | OFF(addr));
+// printf("addr:%x\n",addr);
+uint32_t pdir = PDX(addr);
+uint32_t ptab = PTX(addr);
+uint32_t offset = OFF(addr);
+// note that PDE is a struct, rather than a uint32_t in the nexus-am
+PDE pde;
+pde.val = paddr_read(PTE_ADDR(cpu.cr3.val) | (pdir << 2), 4);
+// guarantee that the page table is present
+assert(pde.present == 1);
+PTE pte;
+pte.val = paddr_read(PTE_ADDR(pde.val) | (ptab << 2), 4);
+// guarantee that the page is present
+if (pte.present == 0)
+{
+printf("addr:%x\n", addr);
 }
-
+assert(pte.present == 1);
+uint32_t paddr = (PTE_ADDR(pte.val) | offset);
+// printf("paddr:%x\n",paddr);
+return paddr;
+}
 uint32_t isa_vaddr_read(vaddr_t addr, int len)
 {
-  if (!cpu.cr0.paging)
-    return paddr_read(addr, len);
-  uint32_t s_vpage_num = VPAGE_NUM(addr);
-  uint32_t e_vpage_num = VPAGE_NUM(addr + len - 1);
-  if (s_vpage_num == e_vpage_num)
-    return paddr_read(page_translate(addr), len);
-  else
-  {
-    uint32_t pre_len = 4096 - OFF(addr);
-    uint32_t suc_len = len - pre_len;
-    uint32_t res = 0;
-    res |= paddr_read(page_translate(e_vpage_num << PTXSHFT), suc_len);
-    res <<= (pre_len << 3);
-    res |= paddr_read(page_translate(addr), pre_len);
-    return res;
-  }
-  return paddr_read(addr, len);
+// return paddr_read(addr, len);
+// now we need to handle the page mapping
+if (cpu.cr0.paging == 0)
+{
+// no paging
+return paddr_read(addr, len);
 }
-
+int start_page_num = addr / PAGE_SIZE;
+int end_page_num = (addr + len - 1) / PAGE_SIZE;
+if (start_page_num == end_page_num)
+{
+// the data is in one page
+paddr_t paddr = page_translate(addr);
+return paddr_read(paddr, len);
+}
+else
+{
+printf("the data is in two pages\n");
+assert(0);
+}
+}
 void isa_vaddr_write(vaddr_t addr, uint32_t data, int len)
 {
-  if (!cpu.cr0.paging)
-  {
-    paddr_write(addr, data, len);
-    return;
-  }
-  uint32_t s_vpage_num = VPAGE_NUM(addr);
-  uint32_t e_vpage_num = VPAGE_NUM(addr + len - 1);
-  if (s_vpage_num == e_vpage_num) // 没有出现跨页
-    paddr_write(page_translate(addr), data, len);
-  else
-  {
-    uint32_t pre_len = 4096 - OFF(addr);
-    uint32_t suc_len = len - pre_len;
-    paddr_write(page_translate(addr), data, pre_len);
-    paddr_write(page_translate(e_vpage_num << PTXSHFT), (data >> (pre_len << 3)), suc_len);
-  }
+if (cpu.cr0.paging == 0)
+{
+// no paging
+paddr_write(addr, data, len);
+return;
+}
+int start_page_num = addr / PAGE_SIZE;
+int end_page_num = (addr + len - 1) / PAGE_SIZE;
+if (start_page_num == end_page_num)
+{
+// the data is in one page
+paddr_t paddr = page_translate(addr);
+paddr_write(paddr, data, len);
+}
+else
+{
+printf("the data is in two pages\n");
+assert(0);
+}
 }
