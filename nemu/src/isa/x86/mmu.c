969b645fc70ce57bd09213a8c2ccb411f160dfd4
tracer-ics2019 <tracer@njuics.org>
2023-06-01 14:27:19 +0800

    >  compile 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:27:19 up 16 days, 14:31,  1 user,  load average: 0.17, 0.07, 0.06 224a1b022043bc3f62acb1226acc7627e9137017

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -1,63 +1,68 @@
 #include "nemu.h"
-#define PTXSHFT        12      // Offset of PTX in a linear address
-#define PDXSHFT        22      // Offset of PDX in a linear address
-#define PDX(va)          (((uint32_t)(va) >> PDXSHFT) & 0x3ff)
-#define PTX(va)          (((uint32_t)(va) >> PTXSHFT) & 0x3ff)
-#define OFF(va)          ((uint32_t)(va) & 0xfff)
-#define ROUNDUP(a, sz)   ((((uintptr_t)a)+(sz)-1) & ~((sz)-1))
-#define ROUNDDOWN(a, sz) ((((uintptr_t)a)) & ~((sz)-1))
-#define PTE_ADDR(pte)    ((uint32_t)(pte) & ~0xfff)
-#define PGADDR(d, t, o)  ((uint32_t)((d) << PDXSHFT | (t) << PTXSHFT | (o)))
-uint32_t page_translate(uint32_t addr)//am.h,vme.c,mmu.c
+
+#define PTXSHFT 12 // Offset of PTX in a linear address
+#define PDXSHFT 22 // Offset of PDX in a linear address
+#define PDX(va) (((uint32_t)(va) >> PDXSHFT) & 0x3ff)
+#define PTX(va) (((uint32_t)(va) >> PTXSHFT) & 0x3ff)
+#define OFF(va) ((uint32_t)(va)&0xfff)
+#define VPAGE_NUM(va) ((uint32_t)(va) >> PTXSHFT)
+#define PTE_ADDR(pte) ((uint32_t)(pte) & ~0xfff)
+
+paddr_t page_translate(vaddr_t addr)
 {
-	uint32_t Dir=(addr>>22),Page=((addr>>12)&1023),Offset=(addr&0xfff);
-	PDE pde;
-	pde.val=paddr_read(PTE_ADDR(cpu.cr3.val)|(Dir<<2),4);
-	if (pde.present==0) assert(0);
-	PTE pte;
-	pte.val=paddr_read(PTE_ADDR(pde.val)|(Page<<2),4);
-	if (pte.present==0) assert(0);
-	uint32_t ret=(PTE_ADDR(pte.val)|Offset);
-	return ret;
-}
-uint32_t isa_vaddr_read(vaddr_t addr, int len) {
-	if (cpu.cr0.paging==0)
-		return paddr_read(addr, len);
-	int s=addr/PAGE_SIZE,t=(addr+len-1)/PAGE_SIZE;
-	if (s!=t)
-	{
-		uint32_t ret=0;
-		for (int i=len-1;i>=0;--i) ret=(ret<<8)|paddr_read(page_translate(addr+i),1);
-		return ret;
-	}
-	else
-	{
-		paddr_t paddr = page_translate(addr);
-        return paddr_read(paddr, len);
-	}
-	return 0;
+  uint32_t dir_index = PDX(addr);
+  uint32_t tab_index = PTX(addr);
+  paddr_t pgdir = cpu.cr3.val;
+  PDE pde;
+  pde.val = paddr_read(pgdir + dir_index * 4, 4);
+  if (!pde.present) {
+    Log("%x %x", addr, pde.val);
+    assert(0);
+  }
+  PTE pte;
+  pte.val = paddr_read((pde.val & ~0xfff) + tab_index * 4, 4);
+  if (!pte.present)
+    assert(0);
+  return (PTE_ADDR(pte.val) | OFF(addr));
 }
 
-void isa_vaddr_write(vaddr_t addr, uint32_t data, int len) {
-
-	if (cpu.cr0.paging==0)
-	{
-		paddr_write(addr, data, len);
-		return;
-	}
-	int s=addr/PAGE_SIZE,t=(addr+len-1)/PAGE_SIZE;
-	if (s!=t)
-	{
-		uint32_t tmp;
-		for (int i=len-1;i>=0;--i)
-			tmp=((data>>(i*8))&0xff),
-			paddr_write(page_translate(addr+i),tmp,1);
-
-	}
-	else
-	{
-		paddr_t paddr = page_translate(addr);
-		paddr_write(paddr,data,len);
-	}
+uint32_t isa_vaddr_read(vaddr_t addr, int len)
+{
+  if (!cpu.cr0.paging)
+    return paddr_read(addr, len);
+  uint32_t s_vpage_num = VPAGE_NUM(addr);
+  uint32_t e_vpage_num = VPAGE_NUM(addr + len - 1);
+  if (s_vpage_num == e_vpage_num) // 没有出现跨页
+    return paddr_read(page_translate(addr), len);
+  else
+  {
+    uint32_t pre_len = 4096 - OFF(addr);
+    uint32_t suc_len = len - pre_len;
+    uint32_t res = 0;
+    res |= paddr_read(page_translate(e_vpage_num << PTXSHFT), suc_len);
+    res <<= (pre_len << 3);
+    res |= paddr_read(page_translate(addr), pre_len);
+    return res;
+  }
+  return paddr_read(addr, len);
 }
 
+void isa_vaddr_write(vaddr_t addr, uint32_t data, int len)
+{
+  if (!cpu.cr0.paging)
+  {
+    paddr_write(addr, data, len);
+    return;
+  }
+  uint32_t s_vpage_num = VPAGE_NUM(addr);
+  uint32_t e_vpage_num = VPAGE_NUM(addr + len - 1);
+  if (s_vpage_num == e_vpage_num) // 没有出现跨页
+    paddr_write(page_translate(addr), data, len);
+  else
+  {
+    uint32_t pre_len = 4096 - OFF(addr);
+    uint32_t suc_len = len - pre_len;
+    paddr_write(page_translate(addr), data, pre_len);
+    paddr_write(page_translate(e_vpage_num << PTXSHFT), (data >> (pre_len << 3)), suc_len);
+  }
+}
