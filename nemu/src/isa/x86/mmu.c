af954ea8337d37ca84b8c508ddd920af84bd4299
tracer-ics2019 <tracer@njuics.org>
2023-06-01 14:02:20 +0800

    >  compile 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:02:20 up 16 days, 14:06,  1 user,  load average: 0.20, 0.10, 0.03 1180ed74e73c0fdf818c3708a2cb4fe3490c415b

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -1,77 +1,63 @@
 #include "nemu.h"
-#define PTXSHFT 12 // Offset of PTX in a linear address
-#define PDXSHFT 22 // Offset of PDX in a linear address
-#define PDX(va) (((uint32_t)(va) >> PDXSHFT) & 0x3ff)
-#define PTX(va) (((uint32_t)(va) >> PTXSHFT) & 0x3ff)
-#define OFF(va) ((uint32_t)(va)&0xfff)
-#define VPAGE_NUM(va) ((uint32_t)(va) >> PTXSHFT)
-#define PTE_ADDR(pte) ((uint32_t)(pte) & ~0xfff)
+#define PTXSHFT        12      // Offset of PTX in a linear address
+#define PDXSHFT        22      // Offset of PDX in a linear address
+#define PDX(va)          (((uint32_t)(va) >> PDXSHFT) & 0x3ff)
+#define PTX(va)          (((uint32_t)(va) >> PTXSHFT) & 0x3ff)
+#define OFF(va)          ((uint32_t)(va) & 0xfff)
+#define ROUNDUP(a, sz)   ((((uintptr_t)a)+(sz)-1) & ~((sz)-1))
+#define ROUNDDOWN(a, sz) ((((uintptr_t)a)) & ~((sz)-1))
+#define PTE_ADDR(pte)    ((uint32_t)(pte) & ~0xfff)
+#define PGADDR(d, t, o)  ((uint32_t)((d) << PDXSHFT | (t) << PTXSHFT | (o)))
+uint32_t page_translate(uint32_t addr)//am.h,vme.c,mmu.c
+{
+	uint32_t Dir=(addr>>22),Page=((addr>>12)&1023),Offset=(addr&0xfff);
+	PDE pde;
+	pde.val=paddr_read(PTE_ADDR(cpu.cr3.val)|(Dir<<2),4);
+	if (pde.present==0) assert(0);
+	PTE pte;
+	pte.val=paddr_read(PTE_ADDR(pde.val)|(Page<<2),4);
+	if (pte.present==0) assert(0);
+	uint32_t ret=(PTE_ADDR(pte.val)|Offset);
+	return ret;
+}
+uint32_t isa_vaddr_read(vaddr_t addr, int len) {
+	if (cpu.cr0.paging==0)
+		return paddr_read(addr, len);
+	int s=addr/PAGE_SIZE,t=(addr+len-1)/PAGE_SIZE;
+	if (s!=t)
+	{
+		uint32_t ret=0;
+		for (int i=len-1;i>=0;--i) ret=(ret<<8)|paddr_read(page_translate(addr+i),1);
+		return ret;
+	}
+	else
+	{
+		paddr_t paddr = page_translate(addr);
+        return paddr_read(paddr, len);
+	}
+	return 0;
+}
 
-uint32_t page_translate(uint32_t addr)
-{
-// printf("addr:%x\n",addr);
-uint32_t pdir = PDX(addr);
-uint32_t ptab = PTX(addr);
-uint32_t offset = OFF(addr);
-// note that PDE is a struct, rather than a uint32_t in the nexus-am
-PDE pde;
-pde.val = paddr_read(PTE_ADDR(cpu.cr3.val) | (pdir << 2), 4);
-// guarantee that the page table is present
-assert(pde.present == 1);
-PTE pte;
-pte.val = paddr_read(PTE_ADDR(pde.val) | (ptab << 2), 4);
-// guarantee that the page is present
-if (pte.present == 0)
-{
-printf("addr:%x\n", addr);
+void isa_vaddr_write(vaddr_t addr, uint32_t data, int len) {
+
+	if (cpu.cr0.paging==0)
+	{
+		paddr_write(addr, data, len);
+		return;
+	}
+	int s=addr/PAGE_SIZE,t=(addr+len-1)/PAGE_SIZE;
+	if (s!=t)
+	{
+		uint32_t tmp;
+		for (int i=len-1;i>=0;--i)
+			tmp=((data>>(i*8))&0xff),
+			paddr_write(page_translate(addr+i),tmp,1);
+
+	}
+	else
+	{
+		paddr_t paddr = page_translate(addr);
+		paddr_write(paddr,data,len);
+	}
 }
-assert(pte.present == 1);
-uint32_t paddr = (PTE_ADDR(pte.val) | offset);
-// printf("paddr:%x\n",paddr);
-return paddr;
-}
-uint32_t isa_vaddr_read(vaddr_t addr, int len)
-{
-// return paddr_read(addr, len);
-// now we need to handle the page mapping
-if (cpu.cr0.paging == 0)
-{
-// no paging
-return paddr_read(addr, len);
-}
-int start_page_num = addr / PAGE_SIZE;
-int end_page_num = (addr + len - 1) / PAGE_SIZE;
-if (start_page_num == end_page_num)
-{
-// the data is in one page
-paddr_t paddr = page_translate(addr);
-return paddr_read(paddr, len);
-}
-else
-{
-printf("the data is in two pages\n");
-assert(0);
-}
-}
-void isa_vaddr_write(vaddr_t addr, uint32_t data, int len)
-{
-if (cpu.cr0.paging == 0)
-{
-// no paging
-paddr_write(addr, data, len);
-return;
-}
-int start_page_num = addr / PAGE_SIZE;
-int end_page_num = (addr + len - 1) / PAGE_SIZE;
-if (start_page_num == end_page_num)
-{
-// the data is in one page
-paddr_t paddr = page_translate(addr);
-paddr_write(paddr, data, len);
-}
-else
-{
-printf("the data is in two pages\n");
-assert(0);
-}
-}
+
