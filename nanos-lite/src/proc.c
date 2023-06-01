f39488b69181c9599a6d35e162f18706b94c264a
tracer-ics2019 <tracer@njuics.org>
2023-06-01 10:51:23 +0800

    >  run 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:51:23 up 16 days, 10:55,  1 user,  load average: 0.72, 0.32, 0.27 85796e1695bbca4974ca5f963e475c3ba0307139

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -13,7 +13,7 @@ void switch_boot_pcb() {
 void hello_fun(void *arg) {
   int j = 1;
   while (1) {
-    Log("Hello World from Nanos-lite for the %dth time!,%s", j,(char *)arg);
+    //Log("Hello World from Nanos-lite for the %dth time!,%s", j,(char *)arg);
     j ++;
     _yield();
   }
@@ -29,8 +29,8 @@ void init_proc() {
   // naive_uload(0, "/bin/bmptest");
 
 
-  context_kload(&pcb[0], hello_fun, "kernel thread 1");
-  context_kload(&pcb[1], hello_fun, "kernel thread 2");
+  // context_kload(&pcb[0], hello_fun, "kernel thread 233");
+  // context_uload(&pcb[1], "/bin/pal", 2, arg, NULL);
 
 
   // context_kload(&pcb[0], hello_fun, "kernel thread 233");
@@ -43,14 +43,14 @@ naive_uload(NULL, "/bin/dummy");
 _Context* schedule(_Context *prev) {
 
     current->cp = prev;
-    if(current==&pcb[0])
-    {
-      current=&pcb[1];
-    }
-    else
-    {
-    current=&pcb[0];
-    }
-    //current = &pcb[0];
+    // if(current==&pcb[0])
+    // {
+    //   current=&pcb[1];
+    // }
+    // else
+    // {
+    // current=&pcb[0];
+    // }
+    current = &pcb[0];
     return current->cp;
 }
