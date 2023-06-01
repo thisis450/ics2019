144b0bea03a6136316da3957f5b7fcedb82a9fa0
tracer-ics2019 <tracer@njuics.org>
2023-06-01 13:23:50 +0800

    >  run 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:23:50 up 16 days, 13:27,  1 user,  load average: 0.16, 0.05, 0.01 ce257e15138ad02b92fbdd877527c1c837842c5f

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -39,42 +39,85 @@
 //   // fs_close(fd);
 //   return elf_head.e_entry;
 // }
+// static uintptr_t loader(PCB *pcb, const char *filename)
+// {
+//   //TODO();
+//   int fd = fs_open(filename, 0, 0);
+//   unsigned char buf[2048];
+//   Elf_Ehdr elf_head;
+//   fs_read(fd, &elf_head, sizeof(Elf_Ehdr));
+//   // ramdisk_read(&elf_head, 0, sizeof(Elf_Ehdr));
+//   Elf_Phdr elf_phentry;
+//   for (int i = 0; i < elf_head.e_phnum; i++)
+//   {
+//     fs_lseek(fd, elf_head.e_phoff + i * elf_head.e_phentsize, SEEK_SET);
+//     fs_read(fd, &elf_phentry, elf_head.e_phentsize);
+//     // ramdisk_read(&elf_phentry, i, sizeof(Elf_Phdr));
+//     if (elf_phentry.p_type == PT_LOAD)
+//     {
+//       int len = 0;
+//       size_t file_off = elf_phentry.p_offset;
+//       unsigned char *mem_addr = (unsigned char *)elf_phentry.p_vaddr;
+//       while (len < elf_phentry.p_filesz)
+//       {
+//         int mov_size = (elf_phentry.p_filesz - len > 2048 ? 2048 : elf_phentry.p_filesz - len);
+//         fs_lseek(fd, file_off, SEEK_SET);
+//         fs_read(fd, buf, mov_size);
+//         // ramdisk_read(buf, file_off, mov_size);
+//         memcpy(mem_addr, buf, mov_size);
+//         file_off += mov_size;
+//         mem_addr += mov_size;
+//         len += mov_size;
+//       }
+//       memset(mem_addr, 0, elf_phentry.p_memsz - len);
+//     }
+//   }
+//   fs_close(fd);
+//   return elf_head.e_entry;
+// }
+
+#define PAGE_SIZE 4096
+Elf_Phdr pHeaders[20];
 static uintptr_t loader(PCB *pcb, const char *filename)
 {
-  //TODO();
   int fd = fs_open(filename, 0, 0);
-  unsigned char buf[2048];
-  Elf_Ehdr elf_head;
-  fs_read(fd, &elf_head, sizeof(Elf_Ehdr));
-  // ramdisk_read(&elf_head, 0, sizeof(Elf_Ehdr));
-  Elf_Phdr elf_phentry;
-  for (int i = 0; i < elf_head.e_phnum; i++)
+  Log("fd : %d, filename : %s", fd, filename);
+  //读取文件头
+  Elf_Ehdr elfHeader;
+  size_t len = fs_read(fd, &elfHeader, sizeof(Elf_Ehdr));
+  assert(len == sizeof(Elf_Ehdr));
+  //读取段头
+  fs_lseek(fd, elfHeader.e_phoff, SEEK_SET);
+  fs_read(fd, pHeaders, elfHeader.e_phentsize * elfHeader.e_phnum);
+  uint32_t paddr, vaddr=0x40000000;
+  int pages = 0;
+  for (int i = 0; i < elfHeader.e_phnum; i++)
   {
-    fs_lseek(fd, elf_head.e_phoff + i * elf_head.e_phentsize, SEEK_SET);
-    fs_read(fd, &elf_phentry, elf_head.e_phentsize);
-    // ramdisk_read(&elf_phentry, i, sizeof(Elf_Phdr));
-    if (elf_phentry.p_type == PT_LOAD)
+    if (pHeaders[i].p_type != PT_LOAD)
+      continue;
+    //将该段读取到制定的内存位置
+    fs_lseek(fd, pHeaders[i].p_offset, SEEK_SET);
+    pages = (pHeaders[i].p_memsz + PAGE_SIZE - 1) / PAGE_SIZE;
+    paddr = (uint32_t)new_page(pages);
+    fs_read(fd, (void *)paddr, pHeaders[i].p_filesz);
+    vaddr = pHeaders[i].p_vaddr;
+    for (int j = 0; j < pages; j++)
     {
-      int len = 0;
-      size_t file_off = elf_phentry.p_offset;
-      unsigned char *mem_addr = (unsigned char *)elf_phentry.p_vaddr;
-      while (len < elf_phentry.p_filesz)
-      {
-        int mov_size = (elf_phentry.p_filesz - len > 2048 ? 2048 : elf_phentry.p_filesz - len);
-        fs_lseek(fd, file_off, SEEK_SET);
-        fs_read(fd, buf, mov_size);
-        // ramdisk_read(buf, file_off, mov_size);
-        memcpy(mem_addr, buf, mov_size);
-        file_off += mov_size;
-        mem_addr += mov_size;
-        len += mov_size;
-      }
-      memset(mem_addr, 0, elf_phentry.p_memsz - len);
+      //需要一页一页的映射
+      // printf("[loader]pa:%x mapped on va:%x\n", paddr, vaddr);
+      _map(&pcb->as, (void *)vaddr, (void *)paddr, _PROT_READ | _PROT_WRITE | _PROT_EXEC);
+      vaddr += PAGE_SIZE;
+      paddr += PAGE_SIZE;
     }
+    // fs_read(fd, (void *)pHeaders[i].p_vaddr, pHeaders[i].p_filesz);
+    //如果出现没对齐的情况把相应的内存区域清0
+    if (pHeaders[i].p_filesz < pHeaders[i].p_memsz)
+      memset((void *)(paddr + pHeaders[i].p_filesz), 0, pHeaders[i].p_memsz - pHeaders[i].p_filesz);
   }
-  fs_close(fd);
-  return elf_head.e_entry;
+  pcb->max_brk = (uintptr_t)vaddr;
+  return elfHeader.e_entry;
 }
+
 void naive_uload(PCB *pcb, const char *filename)
 {
   uintptr_t entry = loader(pcb, filename);
@@ -100,5 +143,5 @@ void context_uload(PCB *pcb, const char *filename, int argc, char *const argv[],
   stack.end = stack.start + sizeof(pcb->stack);
 
   pcb->cp = _ucontext(&pcb->as, stack, stack, (void *)entry, argc, argv, envp);
-  // Log("%x %x", pcb->cp, pcb->cp->as);
+
 }
