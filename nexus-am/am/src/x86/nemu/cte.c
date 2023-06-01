f31f9166a76fb45bf7ebb623d6e315c4bcae6ebc
tracer-ics2019 <tracer@njuics.org>
2023-06-01 08:09:27 +0800

    >  run 2012159 ubuntu Linux VM-8-6-ubuntu 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:09:27 up 16 days,  8:13,  1 user,  load average: 0.20, 0.22, 0.14 9c32826c7d489ed7a0677e7eb82f7874ef1e491

--- nexus-am/am/src/x86/nemu/cte.c
+++ nexus-am/am/src/x86/nemu/cte.c
@@ -69,13 +69,14 @@ int _cte_init(_Context*(*handler)(_Event, _Context*)) {
 
 _Context *_kcontext(_Area stack, void (*entry)(void *), void *arg) {
 
-  void **arg_stack = stack.end - sizeof(void *);
-  *arg_stack = arg;
-  _Context *new_context = stack.end - sizeof(_Context) - sizeof(void *) * 2;
-  new_context->eip = (uintptr_t)entry;
-  new_context->cs = 8;
-  new_context->eflags = 0x202;
-  return new_context;
+_Context *c=(_Context*)(stack.end-sizeof(_Context)-8);
+  void ** arg_stack=(void**)(stack.end-4);
+  *arg_stack=arg;
+  printf("%s",(char*)arg);
+c->eip=(uintptr_t)entry;
+c->eflags=0x2;
+c->cs=8;
+return c;
 }
 
 void _yield() {
